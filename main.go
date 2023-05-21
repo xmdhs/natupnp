72548f38b605389aef2c0d4abc9dfddc737d5755
xmdhs <xmdhss@gmail.com>
2023-05-22 01:16:34 +0800

    use stunprotocol.org

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ var (
 )
 
 func init() {
-	flag.StringVar(&stun, "s", "stun.sipnet.com:3478", "stun")
+	flag.StringVar(&stun, "s", "stunserver.stunprotocol.org:3478", "stun")
 	flag.StringVar(&localAddr, "l", "", "local addr")
 	flag.StringVar(&port, "p", "8086", "port")
 	flag.StringVar(&target, "d", "", "forward to target host")
